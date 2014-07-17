1a41c43bc7d2ef620e42fad5665a33c6870107a7
Petroula Theodoridou <Petroula@users.noreply.github.com>
2014-07-18 01:45:13 +0300

    Update WelcomeActivity.java
    Swipe

--- Android/WelcomeActivity.java
+++ Android/WelcomeActivity.java
@@ -1,34 +1,33 @@
 package com.example.tictacpawfinal;
 
-import android.app.Fragment;
-import android.app.Activity;
+import android.app.*;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.TextView;
+import android.view.*;
+import android.view.GestureDetector.OnGestureListener;
+import android.view.View.*;
+import android.widget.*;
 
-public class WelcomeActivity extends Activity implements OnClickListener {
+public class WelcomeActivity extends Activity implements OnClickListener, OnGestureListener {
 
 	Button button15, button16;
 	TextView textView0;
+	GestureDetector detector;
 	
-	 @Override
-	    protected void onCreate(Bundle savedInstanceState) {
+	 @SuppressWarnings("deprecation")
+	@Override
+	 protected void onCreate(Bundle savedInstanceState) {
 	    	super.onCreate(savedInstanceState);
 	    	setContentView(R.layout.welcome_screen);
 	    	
-	    	button15 = (Button)findViewById(R.id.button15);
+	    	button15 = (Button)findViewById(R.id.button15);	 
 	    	button16 = (Button)findViewById(R.id.button16);
 	    	
 	    	button15.setOnClickListener(this);
-		    button16.setOnClickListener(this);
+	    	button16.setOnClickListener(this);
 		    
+	    	detector = new GestureDetector(this);
+	    	
 		    if (savedInstanceState == null) {
 	        	getFragmentManager().beginTransaction()
 	        	.add(R.id.container, new PlaceholderFragment()).commit();
@@ -79,9 +78,52 @@ public class WelcomeActivity extends Activity implements OnClickListener {
 		 if(v.getId()==R.id.button15) {
 			 Intent changeView = new Intent(getApplicationContext(), MainActivity.class);
 			 startActivity(changeView);
+		 } else if(v.getId()==R.id.button16) {
+			 Intent changeView = new Intent(getApplicationContext(), InstructionsActivity.class);
+			 startActivity(changeView);	     
+		   
 		 }
+		 
+		 
+		 
  	}
+
+	@Override
+	public boolean onDown(MotionEvent e) {
+		return false;
+	}
+
+	@Override
+	public void onShowPress(MotionEvent e) {}
+
+	@Override
+	public boolean onSingleTapUp(MotionEvent e) {
+		return false;
+	}
+
+	@Override
+	public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+		return false;
+	}
+
+	@Override
+	public void onLongPress(MotionEvent e) {}
+
+	@Override
+	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+		if(e1.getX()>e2.getX()) {		
+			MainActivity.recognizeScreen=2;
+			/** Changes to Instructions screen */  	
+       	    Intent changeToInstructions = new Intent(getApplicationContext(), InstructionsActivity.class);
+         	changeToInstructions.putExtra("recognizeScreen", 2);
+			startActivity(changeToInstructions);
+			return true;
+		}
+		return false;
+	}
 	
- 	   	
+	public boolean onTouchEvent(MotionEvent ev) {
+		return detector.onTouchEvent(ev);	
+	}   	
 	    	
 }
